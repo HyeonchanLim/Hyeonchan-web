63f79262e23a27699e4dd0203f2df838f29fef9b
HOME_PC <lhc0366@naver.com>
2024-10-09 22:40:32 +0900

    aa

---
+++ src/main/java/com/home/ch05/While01.java
@@ -0,0 +1,15 @@
+package com.home.ch05;
+
+public class While01 {
+    public static void main(String[] args) {
+        int i=2 , j;
+        while(i<10){
+            j=1;
+            while(j<10){
+                System.out.println(i + "x" + j + "=" + (i*j));
+                j++;
+            }
+            i++;
+        }
+    }
+}
