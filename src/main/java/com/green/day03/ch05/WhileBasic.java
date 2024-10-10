a4747f23138a16efe38273447dd31473850c2c79
LimHyeonChan <lhc0240@gmail.com>
2024-10-10 09:39:04 +0900

    first

---
+++ src/main/java/com/green/day03/ch05/WhileBasic.java
@@ -0,0 +1,12 @@
+package com.green.day03.ch05;
+
+public class WhileBasic {
+    public static void main(String[] args) {
+        int n = 0;
+        while (n < 5) {
+            System.out.println("I Like Java" + n);
+            n ++;
+        }
+        System.out.println("----");
+    }
+}
