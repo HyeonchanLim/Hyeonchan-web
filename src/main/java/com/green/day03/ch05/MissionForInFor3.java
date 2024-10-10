26db57e6eb64edf8527080b8e01a98374fa9b472
LimHyeonChan <lhc0240@gmail.com>
2024-10-10 17:58:41 +0900

    first

--- src/main/java/com/green/day03/ch05/MissionForInFor3.java
+++ src/main/java/com/green/day03/ch05/MissionForInFor3.java
@@ -3,8 +3,21 @@ package com.green.day03.ch05;
 public class MissionForInFor3 {
     public static void main(String[] args) {
         int star = (int)(Math.random()*6)+3; // 3~8사이 랜덤 숫자
-        for ()
+        for (int i=1; i<=star; i++) {
+            for (int j = 1; j <= star; j++) {
+            System.out.print("*");
+            }
+        System.out.println();
+        }
+        System.out.println("----");
 
-        System.out.println(star);
+
+        int len = star * star;
+        for ( int i = 1; i <= len; i++) {
+            System.out.print("*");
+            if (i % star == 0) {
+                System.out.println();
+            }
+        }
     }
 }
