b8be36e8e612a030d91170f6fe66c8daf066d572
LimHyeonChan <lhc0240@gmail.com>
2024-10-11 17:38:48 +0900

    first

---
+++ src/main/java/com/green/study/Gugudan.java
@@ -0,0 +1,12 @@
+package com.green.study;
+
+public class Gugudan {
+    public static void main(String[] args) {
+        for(int i=1; i<=5; i++) {
+            for (int j = 1; j <= 5; j++ ) {
+                System.out.printf("%d x %d = %d\n", i, j, (i * j));
+            }
+        }
+        System.out.println("끝");
+    }
+}
