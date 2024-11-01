ec659d6da45430c6591904531dee3feff895ff9a
LimHyeonChan <lhc0240@gmail.com>
2024-11-01 19:56:09 +0900

    first

---
+++ src/main/java/com/green/study/CodingTest/ArrayLength.java
@@ -0,0 +1,17 @@
+package com.green.study.CodingTest;
+
+public class ArrayLength {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+    }
+}
+class Solution {
+    public int[] solution(String[] strlist) {
+        int[] answer = new int[strlist];
+
+        if(){
+
+        }
+        return answer;
+    }
+}
