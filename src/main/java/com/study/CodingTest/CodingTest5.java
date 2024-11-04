37f5b138994629d912e8b84e8cc58d6e33702fa3
LimHyeonChan <lhc0240@gmail.com>
2024-11-04 21:58:25 +0900

    first

---
+++ src/main/java/com/study/CodingTest/CodingTest5.java
@@ -0,0 +1,13 @@
+package com.study.CodingTest;
+
+import java.util.Arrays;
+
+public class CodingTest5 {
+    class Solution {
+        public int[] solution(int[] array) {
+            int[] answer = {};
+            Arrays.sort();
+            return answer;
+        }
+    }
+}
