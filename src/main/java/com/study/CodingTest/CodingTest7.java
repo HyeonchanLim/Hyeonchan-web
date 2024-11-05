10c779e08c8906edc7bdedd1b78fe7058572cc72
LimHyeonChan <lhc0240@gmail.com>
2024-11-05 21:54:24 +0900

    first

--- src/main/java/com/study/CodingTest/CodingTest7.java
+++ src/main/java/com/study/CodingTest/CodingTest7.java
@@ -2,6 +2,15 @@ package com.study.CodingTest;
 
 public class CodingTest7 {
     class Solution {
-        
+        public String solution(String my_string, int n) {
+            StringBuilder answer = new StringBuilder();
+            for (int i = 0; i < my_string.length(); i++) {
+                char ch = my_string.charAt(i);
+                for (int j = 0; j < n; j++) {
+                    answer.append(ch);
+                }
+            }
+            return answer.toString();
+        }
     }
 }
