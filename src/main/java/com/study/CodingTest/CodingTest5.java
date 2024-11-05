a9edf408feac5bd90e0a56a4edad9b0f972543e9
LimHyeonChan <lhc0240@gmail.com>
2024-11-05 20:35:10 +0900

    first

--- src/main/java/com/study/CodingTest/CodingTest5.java
+++ src/main/java/com/study/CodingTest/CodingTest5.java
@@ -1,12 +1,19 @@
 package com.study.CodingTest;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class CodingTest5 {
     class Solution {
         public int[] solution(int[] array) {
-            int[] answer = {};
-            Arrays.sort();
+            int[] answer = {0,0};
+            for (int i=0; i<array.length; i++){
+                if(answer[0] < array[i]){
+                    answer[0] = array[i];
+                    answer[1] = i;
+                }
+            }
+
             return answer;
         }
     }
